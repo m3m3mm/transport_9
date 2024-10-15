fe09f0f63693c78fc303aec19164a0446df909ed
m3m3mm <153277267+m3m3mm@users.noreply.github.com>
2024-10-15 13:23:34 +0200

    Update transport_catalogue.h

--- transport_catalogue.h
+++ transport_catalogue.h
@@ -1,3 +1,5 @@
+
+
 #pragma once
 
 #include <string>
@@ -28,17 +30,6 @@ struct BusInfo {
     double curvature;
 };
 
-namespace detail {
-    struct PairHash {
-        template <class T1, class T2>
-        std::size_t operator()(const std::pair<T1, T2>& p) const {
-            auto h1 = std::hash<T1>{}(p.first);
-            auto h2 = std::hash<T2>{}(p.second);
-            return h1 ^ h2; // combine hash values
-        }
-    };
-}
-
 class TransportCatalogue {
 public:
     void AddStop(const std::string& name, const geo::Coordinates& coordinates);
@@ -51,11 +42,30 @@ public:
     bool HasStop(const std::string& stop_name) const;
 
 private:
-    std::unordered_map<std::string, Stop> stops_;
-    std::unordered_map<std::string, Bus> buses_;
-    std::unordered_map<std::string, std::unordered_map<std::string, int>> stop_distances_;
-    std::unordered_map<std::string, std::unordered_set<std::string>> stop_to_buses_;
-    // Other private members...
+    struct StopPair {
+        const Stop* from;
+        const Stop* to;
+
+        bool operator==(const StopPair& other) const {
+            return from == other.from && to == other.to;
+        }
+    };
+
+    struct StopPairHasher {
+        std::size_t operator()(const StopPair& pair) const {
+            const std::size_t h1 = std::hash<const Stop*>{}(pair.from);
+            const std::size_t h2 = std::hash<const Stop*>{}(pair.to);
+            return h1 ^ (h2 << 1);
+        }
+    };
+
+    std::deque<Stop> stops_;
+    std::deque<Bus> buses_;
+    std::unordered_map<std::string, const Stop*> stopname_to_stop_;
+    std::unordered_map<std::string, const Bus*> busname_to_bus_;
+    std::unordered_map<const Stop*, std::set<std::string>> stop_to_buses_;
+    std::unordered_map<StopPair, int, StopPairHasher> distances_;
+
 };
 
 } // namespace transport_catalogue
